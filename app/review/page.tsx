1aa50a93f052843a52982dd1e2b1dfbaf039039d
Jude-Akede <ee.akede@gmail.com>
2026-09-15 17:49:48 +0100

    Add successor review route

---
+++ app/review/page.tsx
@@ -0,0 +1,10 @@
+import { Shell } from "@/components/shell";
+import { SuccessorReviewPanel } from "@/components/successor-review-panel";
+
+export default function ReviewPage() {
+  return (
+    <Shell>
+      <SuccessorReviewPanel />
+    </Shell>
+  );
+}
