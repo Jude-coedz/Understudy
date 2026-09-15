3a7bc139970a6d452142537001a847a0458049e5
Jude-Akede <ee.akede@gmail.com>
2026-09-15 18:01:54 +0100

    Add GitHub integration route

---
+++ app/integrations/github/page.tsx
@@ -0,0 +1,10 @@
+import { GithubEvidenceImport } from "@/components/github-evidence-import";
+import { Shell } from "@/components/shell";
+
+export default function GithubIntegrationPage() {
+  return (
+    <Shell>
+      <GithubEvidenceImport />
+    </Shell>
+  );
+}
